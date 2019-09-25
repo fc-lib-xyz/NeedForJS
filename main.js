c74be62378202d7768165f6bed57f50eb9b00888
fc-lib <yura@fc-lib.xyz>
2019-09-25 15:08:12 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -27,7 +27,10 @@ document.addEventListener('keyup', stopRun);
 //функции
 function startGame() {
 	start.classList.add('hide');
-
+	gameArea.innerHTML = '';
+	car.style.left = '125px';
+	car.style.top = 'auto';
+	car.style.bottom = '10px';
 	for(let i = 0; i < getQuantityElements(100); i++){
 		const line = document.createElement('div');
 		line.classList.add('line');
@@ -41,10 +44,12 @@ function startGame() {
 		enemy.y = -100 * setting.traffic * (i + 1);
 		enemy.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px';
 		enemy.style.top = enemy.y + 'px';
-		enemy.style.background = 'transparent url("./image/enemy.png") center / cover no-repeat'
+		enemy.style.background = 'transparent url("./image/enemy.png") center / cover no-repeat';
 		gameArea.appendChild(enemy);
 	}
+	
 	setting.start = true;
+	setting.score = 0;
 	gameArea.appendChild(car);
 	setting.x = car.offsetLeft;
 	setting.y = car.offsetTop;
@@ -53,6 +58,8 @@ function startGame() {
 function playGame(){
 	console.log("playGame");
 	if (setting.start) {
+		setting.score += setting.speed;
+		score.innerHTML= 'score<br>' + setting.score;
 		moveRoad();
 		moveEnemy();
 		if (keys.ArrowLeft && setting.x > 0) {
@@ -96,6 +103,22 @@ function moveRoad() {
 function moveEnemy(){
 	let enemy = document.querySelectorAll('.enemy');
 	enemy.forEach(function(item){
+		let carRect = car.getBoundingClientRect();
+		let enemyRect = item.getBoundingClientRect();
+		
+		
+		/* дополнение в условие по 7 пикселей по всерху и снизу,
+		 *  и по 5 пикселя слева и справа. 
+		 * Машинка меньше всего изображения 
+		 */
+		if (carRect.top + 14 <= enemyRect.bottom && 
+		carRect.left + 10 <= enemyRect.right &&
+		carRect.right - 10 >= enemyRect.left &&
+		carRect.bottom  - 14 >= enemyRect.top){
+			setting.start = false;
+			start.classList.remove('hide');
+			start.style.top = score.offsetHeight;
+		}
 		item.y += setting.speed / 2;
 		item.style.top = item.y + 'px';
 
